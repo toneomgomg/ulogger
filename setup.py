658296015ab8cff3fabc7e5da1c27463704df842
Thomas Hellström <rel@xed.se>
2022-09-09 10:48:32 +0200

    Initial commit

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as file:
+    long_description = file.read()
+
+setup(
+    name="ulogger",
+    version='0.0.1',
+    description="Simple UDP logger.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="Tone Hellström, Henrik Pira",
+    author_email="tone.hellstrom@inter.ikea.com, henrik.pira@inter.ikea.com",
+    packages=find_packages(exclude=("tests",)),
+    include_package_data=True,
+)
